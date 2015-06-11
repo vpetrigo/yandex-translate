e55802f8469f2aeef7edc6ea751c821397a7844a
Vladimir Petrigo <taenaru@gmail.com>
2015-06-11 11:11:09 +0300

    Add logic for storing available translation directions
    For processing json responses nlogmann json library used. Response from Yandex Translator API for available dirs is:
    {
        "dirs" : "az-en", "fr-ge", "ge-lt" ...
    }
    By using json library functionality we can process that responce as the array of string. Each element from this array is cleared from json quotes, and splited in pair.

--- src/Ya_translate.cpp
+++ src/Ya_translate.cpp
@@ -1,7 +1,72 @@
 #include "Ya_translate.hpp"
+#include <sstream>
+#include <algorithm>
+#include <cctype>
 
 namespace Ya_translate {
+    using json = nlohmann::json;
+    
+    /* Function prototypes */
+    
+    // An available language response from Yandex Translator is like that :
+    // en-fr, en-ge, en-ru ...
+    // So, here we need to split it by a delimiter (in the case above the delimiter is '-')
+    static std::pair<std::string, std::string> split_lang(const std::string& langs, const char delim);
+    
+    // Function for deleting quotes from json responce
+    // Accept json string, for example "english-russian" and delete \" quote from the string
+    static void delete_quotes(std::string& json_str);
+        
+    /* Variables */
+    
+    // The API link for getting available translation directions
     const std::string Ya_tr::get_langs_link = "https://translate.yandex.net/api/v1.5/tr.json/getLangs";
+    // The API link for detecting language of passed string
     const std::string Ya_tr::detec_lang_link = "https://translate.yandex.net/api/v1.5/tr.json/detect";
+    // The API link for getting translation
     const std::string Ya_tr::translate_link = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+    
+    /* Definitions */
+    
+    size_t Ya_tr::handle_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+        json data;
+        std::istringstream buf { static_cast<char *> (buffer) };
+        
+        buf >> data;
+        
+        static_cast<Ya_tr *> (userp)->data = data;
+        
+        return size * nmemb;
+    }
+    
+    
+    std::vector<std::pair<std::string, std::string>>& Ya_tr::get_langs(json::value_type& dirs, const char delim) {
+            std::vector<std::pair<std::string, std::string>> *langs = new std::vector<std::pair<std::string, std::string>>;
+            
+            for (auto elem : dirs) {
+                std::string from_to { elem.dump() };
+                
+                delete_quotes(from_to);                
+                langs->push_back(split_lang(from_to, delim));
+            }
+            
+            return *langs;
+        }
+    
+    
+    static std::pair<std::string, std::string> split_lang(const std::string& langs, const char delim) {      
+        std::string from;
+        std::string to;
+        
+        int delim_pos = s.find(delim);
+        
+        from = s.substr(0, delim_pos);
+        to = s.substr(delim_pos + 1);
+        
+        return std::pair<std::string, std::string> {from, to};
+    }
+    
+    static void delete_quotes(std::string& json_str) {      
+        json_str.erase(std::remove(json_str.begin(), json_str.end() + 1, json_quotes), json_str.end());
+    }
 }
--- src/Ya_translate.hpp
+++ src/Ya_translate.hpp
@@ -4,9 +4,17 @@
 namespace Ya_translate {
     using json = nlohmann::json;
     
+    // Define standart json quotes that can be found in responses
+    // {
+    //      "dirs" : "en-ru", "ru-fr", ... <- here we have array of strings
+    //      "code" : 200
+    // }
+    constexpr char json_quotes = '\"';
+    
+    // Class for throwing error if something wrong with an API-key
     class Bad_apikey {
     public:
-        Bad_apikey() {}
+        Bad_apikey() : err{"Bad API-key"} {}
         Bad_apikey(const std::string& s) : err{s} {}
         
         const std::string& get_err() const {
@@ -16,10 +24,11 @@ namespace Ya_translate {
         std::string err;
     };
     
+    // Class for working with Yandex Translator API
     class Ya_tr {
-    public:
+    public:        
         explicit Ya_tr(const std::string& a_k) : api_key{a_k} {
-            if (!api_key.size()) {
+            if ( !api_key.size() ) {
                 throw Bad_apikey{"You did not pass your API-key"};
             }
             
@@ -27,23 +36,40 @@ namespace Ya_translate {
             curl_easy_setopt(ya_h, CURLOPT_URL, get_langs_link.c_str());
             std::string key_f = (static_cast<std::string> ("key=") + a_k);
             curl_easy_setopt(ya_h, CURLOPT_POSTFIELDS, key_f.c_str());
-            curl_easy_setopt(ya_h, CURLOPT_CAINFO, R"(C:\msys64\usr\ssl\certs\ca-bundle.crt)"); // Need to be a path with certificate for https session
+            curl_easy_setopt(ya_h, CURLOPT_CAINFO, R"(..\certs\ca-bundle.crt)"); // Need to be a path with the certificate for https session
+            curl_easy_setopt(ya_h, CURLOPT_WRITEFUNCTION, handle_data);
+            curl_easy_setopt(ya_h, CURLOPT_WRITEDATA, this);
             CURLcode res = curl_easy_perform(ya_h);
             
-            if (res != CURLE_OK) {
+            if ( res != CURLE_OK ) {
                 std::cout << "Something went wrong\n";
+            }
+            
+            avail_lang = get_langs(data["dirs"], lang_delim);
+            
+            for (auto i : avail_lang) {
+                if (i.first == "ru") {
+                    std::cout << "from: " << i.first << " to: "
+                        << i.second << std::endl;
+                }
             }
         }
         
         ~Ya_tr() {
             curl_easy_cleanup(ya_h);
         }
-         
+        
         static const std::string get_langs_link;
         static const std::string detec_lang_link;
         static const std::string translate_link;
     private:
-        const std::string& api_key;
+        static size_t handle_data(void *buffer, size_t size, size_t nmemb, void *userp);      
+        std::vector<std::pair<std::string, std::string>>& get_langs(json::value_type& dirs, const char delim);
+        
+        const std::string api_key;
+        char lang_delim { '-' };
+        json data;
+        std::vector<std::pair<std::string, std::string>> avail_lang;
         CURL *ya_h;
     };
 }
